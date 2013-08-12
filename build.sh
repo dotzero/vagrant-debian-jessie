eb5727ab468183d841db85072e5da4d6bf145a05
dotzero <mail@dotzero.ru>
2013-08-12 10:39:14 +0400

    bug fix race condition while installing VBoxAdditions

--- build.sh
+++ build.sh
@@ -201,6 +201,7 @@ if ! VBoxManage showvminfo "${BOX}" >/dev/null 2>/dev/null; then
   chmod 600 "${FOLDER_BUILD}/id_rsa"
 
   # install virtualbox guest additions
+  sleep 10
   ssh -i "${FOLDER_BUILD}/id_rsa" -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -p 2222 vagrant@127.0.0.1 "sudo mount /dev/cdrom /media/cdrom; sudo sh /media/cdrom/VBoxLinuxAdditions.run -- --force; sudo umount /media/cdrom; sudo shutdown -h now"
   echo -n "Waiting for machine to shut off "
   while VBoxManage list runningvms | grep "${BOX}" >/dev/null; do
