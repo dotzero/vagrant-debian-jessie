9b1bf7d566d63416fec04f0b0922f44ac407ec1f
Martin Jansen <martin@divbyzero.net>
2014-10-23 22:33:05 +0200

    Bump to Debian 7.7.0

--- build.sh
+++ build.sh
@@ -23,8 +23,8 @@ set -o errexit
 
 # Configurations
 BOX="debian-wheezy-64"
-ISO_URL="http://cdimage.debian.org/debian-cd/7.6.0/amd64/iso-cd/debian-7.6.0-amd64-netinst.iso"
-ISO_MD5="8a3c2ad7fd7a9c4c7e9bcb5cae38c135"
+ISO_URL="http://cdimage.debian.org/debian-cd/7.7.0/amd64/iso-cd/debian-7.7.0-amd64-netinst.iso"
+ISO_MD5="0b31bccccb048d20b551f70830bb7ad0"
 
 # location, location, location
 FOLDER_BASE=$(pwd)
